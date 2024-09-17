9299f1854eb8257498b9dcbb2c7f6c08c8bf43d0
MD KARAMAT ALI <93656306+MdKaramatAli@users.noreply.github.com>
2024-09-18 01:12:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ dark.addEventListener("click", function(){
     document.body.style.color = "#111";
     active.style.borderColor = "#000";
 
-    person_photo.src = "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    person_photo.src = "";
 })
 
 light.addEventListener("click", function(){
